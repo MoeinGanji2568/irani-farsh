9d569084dc4241a1ffc28ca5260b40bafd49f5f3
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-28 18:50:41 +0330

    update: change structure of header and be a responsive

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,32 +1,34 @@
-import {
-  ArrowLeftEndOnRectangleIcon,
-  ShoppingCartIcon,
-} from "@heroicons/react/24/outline";
-import Button from "../../ui/Button";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import HeadersButton from "./HeadersButton";
 import HeaderSearchInput from "./HeaderSearchInput";
-import NavigationSection from "./NavigationSection";
+import NavigationSection, { CustomLink } from "./NavigationSection";
+import Button from "../../ui/Button";
+import { useState } from "react";
+import Drawer from "../../ui/Drawer";
 
 const Header = () => {
+  const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false);
   return (
     <div className="holder">
       <div className="flex items-center justify-between py-5">
         <div className="flex items-center gap-5">
-          <h1 className="text-red text-2xl font-bold">ایرانی فرش</h1>
-          <HeaderSearchInput />
-        </div>
-        <div className="flex gap-3">
-          <Button variant="outline">
-            <ShoppingCartIcon className="size-5" />
+          <Button
+            variant="secondary"
+            className="block md:hidden"
+            onClick={() => setIsOpenDrawer(true)}
+          >
+            <Bars3Icon className="size-5" />
           </Button>
-          <Button variant="outline">
-            <div className="flex items-center gap-2">
-              <ArrowLeftEndOnRectangleIcon className="size-6" />
-              <span> ورود/ثبت نام</span>
-            </div>
-          </Button>
+          <h1 className="text-red text-lg md:text-2xl font-bold">ایرانی فرش</h1>
+          <HeaderSearchInput extraClass="hidden md:block" />
         </div>
+        <HeadersButton />
       </div>
       <NavigationSection />
+      <HeaderSearchInput extraClass="block md:hidden mx-auto" />
+      <Drawer open={isOpenDrawer} onClose={() => setIsOpenDrawer(false)}>
+        <CustomLink />
+      </Drawer>
     </div>
   );
 };
