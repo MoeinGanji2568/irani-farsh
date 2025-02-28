4345013f011e898e2d392eac3f0cd7f975606742
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-28 17:32:52 +0330

    add: navlinks data

---
+++ src/constants/NavLinks.tsx
@@ -0,0 +1,18 @@
+export const NavLinks = [
+  {
+    label: "صفحه اصلی",
+    path: "/",
+  },
+  {
+    label: "وبلاگ",
+    path: "/blog",
+  },
+  {
+    label: "تماس با ما",
+    path: "/contact-us",
+  },
+  {
+    label: "درباره ما",
+    path: "/about-us",
+  },
+];
