cbd2c7b8f62c923800d0f3f957fb8bc5d8af04fc
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-28 18:49:09 +0330

    update: add extraClass prop and change some styles for responsive

--- src/components/common/header/HeaderSearchInput.tsx
+++ src/components/common/header/HeaderSearchInput.tsx
@@ -1,13 +1,18 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { FC } from "react";
 
-const HeaderSearchInput = () => {
+interface HeaderSearchInputProps {
+  extraClass?: string;
+}
+
+const HeaderSearchInput: FC<HeaderSearchInputProps> = ({ extraClass = "" }) => {
   return (
-    <div className="relative text-secondary-500">
+    <div className={`${extraClass} relative text-secondary-500`}>
       <MagnifyingGlassIcon className="size-5 absolute right-3 top-3" />
       <input
         type="search"
         placeholder="جستجوی فرش"
-        className="min-w-[432px] p-3 rounded-lg bg-secondary-200 indent-7"
+        className="w-full md:min-w-[400px] lg:min-w-[432px] p-3 rounded-lg bg-secondary-200 indent-7"
       />
     </div>
   );
