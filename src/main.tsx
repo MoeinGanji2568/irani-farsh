eacae3e22224ad20b8b8db7194c8c8e96ae36603
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-02-28 17:08:55 +0330

    add: add Yekan font in project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./app/App.tsx";
+import "./assets/css/fonts.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
