6ed7029cae1f5c804db852945daf3954419a5210
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-03-08 17:26:11 +0330

    update: change structure of Main Layout

--- src/core/layout/MainLayout.tsx
+++ src/core/layout/MainLayout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/common/header";
+import Footer from "../../components/common/footer";
 
 const MainLayout = () => {
   return (
-    <div className="h-screen px-3">
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <div className="px-3">
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 };
 
