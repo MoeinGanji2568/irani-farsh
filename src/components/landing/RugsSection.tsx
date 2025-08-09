fa26c5817ee249c6aceeb060e1d904c875288fe5
MoeinGanji2568 <moeinganji2004@gmail.com>
2025-08-09 16:33:15 +0330

    feat: enhance RugsSection component with improved responsive design and type safety for rug mapping

--- src/components/landing/RugsSection.tsx
+++ src/components/landing/RugsSection.tsx
@@ -3,6 +3,7 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { getCarpet } from "../../services/api/carpets";
+import { Carpet } from "../../types/carpets/carpet.types";
 import Cards from "../common/Cards/Cards";
 import Button from "../ui/Button";
 
@@ -11,6 +12,7 @@ const RugsSection = () => {
     queryKey: ["carpets"],
     queryFn: () => getCarpet(1, 4),
   });
+
   return (
     <div className="my-16 max-w-[1200px] mx-auto overflow-hidden px-4">
       <div className="flex justify-between items-center mt-4">
@@ -25,10 +27,20 @@ const RugsSection = () => {
       </div>
 
       <div className="block lg:hidden mt-8">
-        <Swiper spaceBetween={16} slidesPerView={1.2}>
-          {rugs?.map((rug) => (
+        <Swiper
+          spaceBetween={8}
+          slidesPerView={1.2}
+          breakpoints={{
+            430: { slidesPerView: 1.4, spaceBetween: 8 },
+            640: { slidesPerView: 2, spaceBetween: 10 },
+            768: { slidesPerView: 2.2, spaceBetween: 12 },
+            900: { slidesPerView: 2.5, spaceBetween: 12 },
+            1022: { slidesPerView: 3, spaceBetween: 12 },
+          }}
+        >
+          {rugs?.map((rug: Carpet) => (
             <SwiperSlide key={rug.id}>
-              <div className="w-full max-w-[260px]">
+              <div className="w-full">
                 <Cards rug={rug} />
               </div>
             </SwiperSlide>
@@ -37,7 +49,7 @@ const RugsSection = () => {
       </div>
 
       <div className="hidden lg:grid grid-cols-4 gap-6 mt-8">
-        {rugs?.map((rug) => (
+        {rugs?.map((rug: Carpet) => (
           <Cards key={rug.id} rug={rug} />
         ))}
       </div>
